b3846150a0dd4e5dcaf738202ca5cd00b8b613ec
igorskyflyer <igor.dvlpr@gmail.com>
2021-07-17 23:13:50 +0200

    Update tests with v.1.1.0 API

--- test/main.test.js
+++ test/main.test.js
@@ -1,28 +1,96 @@
 const chai = require('chai').assert
-const { charsInString } = require('../main')
+const { charsInString, Position, stringsInString } = require('../main')
 
 describe('🧪 Chars in Str tests 🧪', () => {
-  it('should return false', () => {
-    chai.isFalse(charsInString([], ''))
+  describe('charsInString()', () => {
+    it('should return false', () => {
+      chai.isFalse(charsInString([], ''))
+    })
+
+    it('should return true', () => {
+      chai.isTrue(charsInString([], 'test'))
+    })
+
+    it('should return false', () => {
+      chai.isFalse(charsInString([':', ','], ''))
+    })
+
+    it('should return true', () => {
+      chai.isTrue(charsInString([':', ','], 'hello:world'))
+    })
+
+    it('should return true', () => {
+      chai.isTrue(charsInString([':', ',', '^'], 'helloworld^'))
+    })
+
+    it('should return true', () => {
+      chai.isTrue(charsInString([':', ',', '^', '*'], 'helloworld*', Position.End))
+    })
+
+    it('should return true', () => {
+      chai.isTrue(charsInString([':', ',', '^'], '^helloworld', Position.Start))
+    })
+
+    it('should return false', () => {
+      chai.isFalse(charsInString([':', ',', '^'], 'helloworld'))
+    })
+
+    it('should return true', () => {
+      chai.isTrue(charsInString([':', ',', '^'], ':helloworld', Position.Start))
+    })
+
+    it('should return false', () => {
+      chai.isFalse(charsInString([':', ',', '^'], 'hell:oworld', Position.Start))
+    })
+
+    it('should return true', () => {
+      chai.isTrue(charsInString([':', ',', '^'], 'helloworld^', Position.End))
+    })
+
+    it('should return false', () => {
+      chai.isFalse(charsInString([':', ',', '^'], 'helloworld^a', Position.End))
+    })
+
+    it('should return false', () => {
+      chai.isFalse(charsInString(['a', 'b', 'c'], 'helloworld'))
+    })
+
+    it('should return true', () => {
+      chai.isTrue(charsInString(['a', 'b', 'o'], 'helloworld'))
+    })
+
+    it('should return false', () => {
+      chai.isFalse(charsInString(['a', 'H', 'O'], 'helloworld'))
+    })
+
+    it('should return true', () => {
+      chai.isTrue(charsInString(['a', 'H', 'o'], 'helloworld', Position.Any, false))
+    })
   })
 
-  it('should return true', () => {
-    chai.isTrue(charsInString([], 'test'))
-  })
+  describe('stringsInString()', () => {
+    it('should return false', () => {
+      chai.isFalse(stringsInString(['abc', 'owom', 'wqp'], 'helloworld'))
+    })
 
-  it('should return false', () => {
-    chai.isFalse(charsInString([':', ','], ''))
-  })
+    it('should return true', () => {
+      chai.isTrue(stringsInString(['abc', 'owom', 'wqp', 'world'], 'helloworld'))
+    })
 
-  it('should return true', () => {
-    chai.isTrue(charsInString([':', ','], 'hello:world'))
-  })
+    it('should return true', () => {
+      chai.isTrue(stringsInString(['abc', 'owom', 'wqp', 'world', 'hel'], 'helloworld', Position.Start))
+    })
 
-  it('should return true', () => {
-    chai.isTrue(charsInString([':', ',', '^'], 'helloworld^'))
-  })
+    it('should return false', () => {
+      chai.isFalse(stringsInString(['abc', 'owom', 'wqp', 'world'], 'helloworld', Position.Start))
+    })
 
-  it('should return false', () => {
-    chai.isFalse(charsInString([':', ',', '^'], 'helloworld'))
+    it('should return false', () => {
+      chai.isFalse(stringsInString(['abc', 'owom', 'wqp', 'world', 'amk'], 'helloworld akm', Position.End))
+    })
+
+    it('should return true', () => {
+      chai.isTrue(stringsInString(['abc', 'owom', 'wqp', 'world', 'wio'], 'helloworld wio', Position.End))
+    })
   })
 })
