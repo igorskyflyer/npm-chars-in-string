5d6280caff1a15a696dcbc9dae81d4562e539039
igorskyflyer <igor.dvlpr@gmail.com>
2025-08-31 15:23:44 +0200

    Migrate to ESM

--- test/index.test.js
+++ test/index.test.js
@@ -1,104 +1,104 @@
-const chai = require('chai').assert
-const { charsInString, Position, stringsInString } = require('../src/index')
+import { assert, describe, it } from 'vitest'
+import { charsInString, Position, stringsInString } from '../src/index.js'
 
 describe('🧪 Chars in Str tests 🧪', () => {
   describe('charsInString()', () => {
     it('should return false', () => {
-      chai.isFalse(charsInString([], ''))
+      assert.isFalse(charsInString([], ''))
     })
 
     it('should return false', () => {
-      chai.isFalse(charsInString([], 'test'))
+      assert.isFalse(charsInString([], 'test'))
     })
 
     it('should return false', () => {
-      chai.isFalse(charsInString([':', ','], ''))
+      assert.isFalse(charsInString([':', ','], ''))
     })
 
     it('should return true', () => {
-      chai.isTrue(charsInString([':', ','], 'hello:world'))
+      assert.isTrue(charsInString([':', ','], 'hello:world'))
     })
 
     it('should return true', () => {
-      chai.isTrue(charsInString([':', ',', '^'], 'helloworld^'))
+      assert.isTrue(charsInString([':', ',', '^'], 'helloworld^'))
     })
 
     it('should return true', () => {
-      chai.isTrue(charsInString([':', ',', '^', '*'], 'helloworld*', Position.End))
+      assert.isTrue(charsInString([':', ',', '^', '*'], 'helloworld*', Position.End))
     })
 
     it('should return true', () => {
-      chai.isTrue(charsInString([':', ',', '^'], '^helloworld', Position.Start))
+      assert.isTrue(charsInString([':', ',', '^'], '^helloworld', Position.Start))
     })
 
     it('should return false', () => {
-      chai.isFalse(charsInString([':', ',', '^'], 'helloworld'))
+      assert.isFalse(charsInString([':', ',', '^'], 'helloworld'))
     })
 
     it('should return true', () => {
-      chai.isTrue(charsInString(['heowm'], 'helloworld'))
+      assert.isTrue(charsInString(['heowm'], 'helloworld'))
     })
 
     it('should return true', () => {
-      chai.isTrue(charsInString([':', ',', '^'], ':helloworld', Position.Start))
+      assert.isTrue(charsInString([':', ',', '^'], ':helloworld', Position.Start))
     })
 
     it('should return false', () => {
-      chai.isFalse(charsInString([':', ',', '^'], 'hell:oworld', Position.Start))
+      assert.isFalse(charsInString([':', ',', '^'], 'hell:oworld', Position.Start))
     })
 
     it('should return true', () => {
-      chai.isTrue(charsInString([':', ',', '^'], 'helloworld^', Position.End))
+      assert.isTrue(charsInString([':', ',', '^'], 'helloworld^', Position.End))
     })
 
     it('should return false', () => {
-      chai.isFalse(charsInString([':', ',', '^'], 'helloworld^a', Position.End))
+      assert.isFalse(charsInString([':', ',', '^'], 'helloworld^a', Position.End))
     })
 
     it('should return false', () => {
-      chai.isFalse(charsInString(['a', 'b', 'c'], 'helloworld'))
+      assert.isFalse(charsInString(['a', 'b', 'c'], 'helloworld'))
     })
 
     it('should return true', () => {
-      chai.isTrue(charsInString(['a', 'b', 'o'], 'helloworld'))
+      assert.isTrue(charsInString(['a', 'b', 'o'], 'helloworld'))
     })
 
     it('should return false', () => {
-      chai.isFalse(charsInString(['a', 'H', 'O'], 'helloworld'))
+      assert.isFalse(charsInString(['a', 'H', 'O'], 'helloworld'))
     })
 
     it('should return true', () => {
-      chai.isTrue(charsInString(['a', 'H', 'o'], 'helloworld', Position.Any, false))
+      assert.isTrue(charsInString(['a', 'H', 'o'], 'helloworld', Position.Any, false))
     })
   })
 
   describe('stringsInString()', () => {
     it('should return false', () => {
-      chai.isFalse(stringsInString(['abc', 'owom', 'wqp'], 'helloworld'))
+      assert.isFalse(stringsInString(['abc', 'owom', 'wqp'], 'helloworld'))
     })
 
     it('should return true', () => {
-      chai.isTrue(stringsInString(['abc', 'owom', 'wqp', 'world'], 'helloworld'))
+      assert.isTrue(stringsInString(['abc', 'owom', 'wqp', 'world'], 'helloworld'))
     })
 
     it('should return true', () => {
-      chai.isTrue(stringsInString(['abc', 'owom', 'wqp', 'world', 'hel'], 'helloworld', Position.Start))
+      assert.isTrue(stringsInString(['abc', 'owom', 'wqp', 'world', 'hel'], 'helloworld', Position.Start))
     })
 
     it('should return true', () => {
-      chai.isTrue(stringsInString(['abc', 'owom', 'wqp', 'world', 'HEL'], 'helloworld', Position.Start, false))
+      assert.isTrue(stringsInString(['abc', 'owom', 'wqp', 'world', 'HEL'], 'helloworld', Position.Start, false))
     })
 
     it('should return false', () => {
-      chai.isFalse(stringsInString(['abc', 'owom', 'wqp', 'world'], 'helloworld', Position.Start))
+      assert.isFalse(stringsInString(['abc', 'owom', 'wqp', 'world'], 'helloworld', Position.Start))
     })
 
     it('should return false', () => {
-      chai.isFalse(stringsInString(['abc', 'owom', 'wqp', 'world', 'amk'], 'helloworld akm', Position.End))
+      assert.isFalse(stringsInString(['abc', 'owom', 'wqp', 'world', 'amk'], 'helloworld akm', Position.End))
     })
 
     it('should return true', () => {
-      chai.isTrue(stringsInString(['abc', 'owom', 'wqp', 'world', 'wio'], 'helloworld wio', Position.End))
+      assert.isTrue(stringsInString(['abc', 'owom', 'wqp', 'world', 'wio'], 'helloworld wio', Position.End))
     })
   })
 })
